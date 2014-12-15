a548dff320ea7501153d9330035f9a5f8ea581a0
Albert Weichselbraun <albert@weichselbraun.net>
2014-12-15 13:59:59 +0100

    add: support for config files.

---
+++ src/davify/config.py
@@ -0,0 +1,14 @@
+from ConfigParser import ConfigParser
+from os.path import expanduser
+
+CONFIG = ConfigParser()
+CONFIG.read(expanduser('~/.davify'))
+
+_g = lambda option, default: CONFIG.get(section='default', option=option) \
+        if CONFIG.has_option(section='default', option=option) else default
+
+FILENAME_PATTERN = _g('filename_pattern', '{random_prefix}{lifetime_str}-{fname}{version_suffix}{ext}')
+FILE_URL_PATTERN = _g('file_url_pattern', '{protocol}://{file_server}{file_path}{random_prefix}{lifetime_str}-{fname}{version_suffix}{ext}')
+MESSAGE = _g('notification_message', '{url}\n(Note: The file will be availalbe for {hours} hours.)')
+
+
